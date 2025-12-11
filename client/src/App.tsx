76085ae48f1b0c7f97b1a0bbd784de7b86a6179f
ashish prajapat <ashishprajapat507@gmail.com>
2025-12-11 17:22:01 +0530

    server routes and tailwindcss  ty and   basic setup for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Page/HomePage";
+import MyProject from "./Page/MyProject";
+import Community from "./Page/Community";
+import Pricing from "./Page/Pricing";
+import Priview from "./Page/Priview";
+import View from "./Page/View";
+import Projects from "./Page/Projects";
 
 const App = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<MyProject />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects/:projectId/:versionId" element={<Projects />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/project/:projectId" element={<Priview />} />
+        <Route path="/view/:projectId" element={<View />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/Page/Priview.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Priview = () => {
+  return <div>Priview</div>;
+};
+
+export default Priview;
